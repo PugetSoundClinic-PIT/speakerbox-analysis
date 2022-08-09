ea807d85eed2d4d397d7cc5eba95316967e02a50
evamaxfield <evamaxfieldbrown@gmail.com>
2022-08-09 19:08:38 +0000

    Deploying to gh-pages from main @ 2b4ff5c6e4c217798e00b9759ddd1e40f836a513 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g7eb67ca',
+    VERSION: '0.1.dev1+g2b4ff5c',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
