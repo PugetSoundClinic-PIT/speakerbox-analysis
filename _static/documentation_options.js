9c5f89602d7cab45ebb448dbcf679c99c39fc455
evamaxfield <evamaxfieldbrown@gmail.com>
2022-08-09 19:14:30 +0000

    Deploying to gh-pages from @ PugetSoundClinic-PIT/speakerbox-analysis@434a6da9c81f9acc808f89e79cdde637f94d4250 🚀

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.1.dev1+g2b4ff5c',
+    VERSION: '0.1.dev1+g434a6da',
     LANGUAGE: 'en',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
